f956be55d3436e5c0d980c72648dfb5404a8c44b
amirhossein <shekooha696@gmail.com>
2020-10-08 15:13:54 +0330

    added styles and also changed some urls

--- cat_news/urls.py
+++ cat_news/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
 from . import views
+
+app_name="cats"
 urlpatterns = [
-   url(r'^$', views.base),
-   url(r'^facts$', views.cat_fact),
+   url(r'^$', views.base, name='home'),
+   url(r'^facts$', views.cat_fact, name='facts'),
 ]
